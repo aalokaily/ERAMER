e44874da759b47a72072ec661d91dee4c5ce710d
aalokaily <aaa10013@engr.uconn.edu>
2017-01-02 04:51:00 -0500

    Update ERAMER.py

--- ERAMER.py
+++ ERAMER.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 import math
@@ -10,9 +11,9 @@ All_weights = defaultdict(float)
 
 if len(sys.argv) != 2:
     print "Please provide a fasta file"
-    sys.exit(1) 
+    sys.exit(1)
 
-peptide_file =  sys.argv[1]
+peptide_file = sys.argv[1]
 
 
 def check_has_lower(s):
@@ -28,8 +29,8 @@ def read_PWM(xls_file):
         workbook = xlrd.open_workbook(xls_file)
     except:
         print "Please store the PWM.xlsx file in the script's directory"
-        sys.exit(1) 
-        
+        sys.exit(1)
+
     ID = ""
     for length_peptide in [9, 10, 11, 12, 13, 14, 15, 16]:
         worksheet = workbook.sheet_by_name('Length ' + str(length_peptide))
@@ -37,7 +38,6 @@ def read_PWM(xls_file):
             for col in range(2, 2 + length_peptide):
                 ID = str(length_peptide)+ "_" + worksheet.cell(rowx=row,colx=1).value + "_" + str(col - 1)
                 All_weights[ID] = worksheet.cell(rowx=row,colx=col).value
-                #print ID, worksheet.cell(rowx=row,colx=col).value
 
         All_weights[str(length_peptide) + "_length_weight"] = worksheet.cell(rowx=24,colx=2).value
         #print length_peptide, worksheet.cell(rowx=24,colx=2).value
@@ -68,21 +68,29 @@ d = 0
 peptide_id = ""
 peptide_seq = ""
 
-print "Peptide ID\tPosition of the N-terminus of precursor \tExpected transported precursor (lengths 9-16)\tTrimming score\n--------------------------------------------------------------------------------------------------------------------------------"
+large = 0
+print "Peptide ID\tPosition of the C-terminus of precursor \tTrimming score, LETP = 9\tTrimming score, LETP = 10\tTrimming score, LETP = 11\tTrimming score, LETP = 12\tTrimming score, LETP = 13\tTrimming score, LETP = 14\tTrimming score, LETP = 15\tTrimming score, LETP = 16\t\n---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 for line in fileinput.input(peptide_file):
     tmp =  line.strip()
+
     #print tmp
     if tmp[0] == ">" and d == 0:
         peptide_id = tmp[1:]
 
     elif tmp[0] == ">" and d != 0:
         peptide_seq.upper()
-        for i in range(0, len(peptide_seq) - 9):
+        for i in range(9, len(peptide_seq) ):
+            large = 0
+            print peptide_id, "\t", i , "\t",
             for prec_length in [9, 10, 11, 12, 13, 14, 15, 16]:
-                mer = peptide_seq[i:i+prec_length]
+                mer = peptide_seq[(i-prec_length):i]
                 if len(mer)  == prec_length:
-                    print peptide_id, "\t", i + 1, "\t", prec_length, "\t", mer, (16 - prec_length) * " ", "\t", compute_specificty(mer)
-
+                 #   if large < compute_specificty(mer):
+                  #      large = compute_specificty(mer)
+                      print compute_specificty(mer), "\t",  #large #compute_specificty(mer)
+                else:
+                      print "0\t",  #large #compute_specificty(mer)
+            print 
         peptide_id = tmp[1:]
         peptide_seq = ""
 
@@ -92,11 +100,19 @@ for line in fileinput.input(peptide_file):
 # process the last peptide
 
 peptide_seq.upper()
-for i in range(0, len(peptide_seq) - 9):
-    for prec_length in [9, 10, 11, 12, 13, 14, 15, 16]:
-        mer = peptide_seq[i:i+prec_length]
-        if len(mer)  == prec_length:
-            print peptide_id, "\t", i + 1, "\t", prec_length, "\t", mer, (16 - prec_length) * " ", "\t", compute_specificty(mer)
+for i in range(9, len(peptide_seq) ):
+            large = 0
+            print peptide_id, "\t", i , "\t",
+            for prec_length in [9, 10, 11, 12, 13, 14, 15, 16]:
+                mer = peptide_seq[(i-prec_length):i]
+                if len(mer)  == prec_length:
+                 #   if large < compute_specificty(mer):
+                  #      large = compute_specificty(mer)
+                      print compute_specificty(mer), "\t",  #large #compute_specificty(mer)
+                else:
+                      print "0\t",  #large #compute_specificty(mer)
+            print 
+
 
 
 
